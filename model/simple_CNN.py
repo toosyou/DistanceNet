6d65963e55661a8ce5056ab2cf739345999bfda0
writerwriter <catchingfire80@gmail.com>
2020-09-08 15:31:17 +0800

    Add: simple CNN for baseline testing

---
+++ model/simple_CNN.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class simple_CNN(nn.Module):
+    def __init__(self, num_layers, in_ch, out_ch, input_length):
+        super(simple_CNN, self).__init__()
+        self.backbone = self.make_layer(num_layers, in_ch)
+
+        self.ap = nn.AvgPool1d(kernel_size=48)
+
+        #self.final1 = nn.Linear(in_ch*(2**num_layers), in_ch*(2**(num_layers-1)))
+        #self.final2 = nn.Linear(in_ch*(2**(num_layers-1)), in_ch*(2**(num_layers-2)))
+        self.final = nn.Linear(in_ch*(2**(num_layers)),1)
+
+    def forward(self, x):
+        y = self.backbone(x)
+        y = self.ap(y)
+        y = self.final(y.flatten(1))
+        #y = self.final2(y)
+        #y = self.final(y)
+        return y
+
+    def make_layer(self, num_layers, in_ch):
+        seq = []
+        for i in range(num_layers):
+            seq.append(nn.Conv1d(in_ch*(2**i), in_ch*(2**(i+1)), kernel_size=3, padding=1))
+            seq.append(nn.BatchNorm1d(in_ch*(2**(i+1))))
+            seq.append(nn.MaxPool1d(kernel_size=2))
+            seq.append(nn.ReLU())
+        return nn.Sequential(*seq)
---
+++ train.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from model import simple_CNN
+from gen_data import gen_distance_peak_data
+import torch.utils.data as Data
+
+def train(batch_size=64):
+    model = simple_CNN.simple_CNN(1, 2, 1, 100)
+    model = nn.DataParallel(model).cuda()
+    model.train()
+
+    num_data = 100000
+
+    X, y = gen_distance_peak_data(num_data=num_data)
+    ds = Data.TensorDataset(torch.tensor(X-0.5, dtype=torch.float32), torch.tensor(np.abs(y), dtype=torch.float32))
+
+    train_len = int(len(ds)*0.6)
+    val_len = int(len(ds)*0.2)
+    test_len = int(len(ds)*0.2)
+    train_set, val_set, test_set = Data.random_split(ds, [train_len, val_len, test_len])
+    train_loader = Data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    val_loader = Data.DataLoader(val_set, batch_size=batch_size, shuffle=True)
+    test_loader = Data.DataLoader(test_set, batch_size=batch_size, shuffle=True)
+    
+    optimizer = torch.optim.Adam(params=model.parameters())
+
+    criterion = torch.nn.L1Loss()
+
+    for epoch in range(1000):
+        model.train()
+
+        total_loss = 0
+        avg_loss = 0
+        fake_loss = 0
+
+        for batch_idx, (inputs, labels) in enumerate(train_loader):
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+            optimizer.zero_grad()
+
+            pred = model(inputs)
+            pred = torch.exp(pred)
+            loss = criterion(pred, labels)
+
+            loss.backward()
+            optimizer.step()
+            
+            total_loss += loss.item()
+            avg_loss += avg_loss_calc(pred, labels).item()
+            #fake_loss += avg_loss_calc(torch.zeros(labels.size()), labels).item()
+        avg_val_loss = evaluate(model, val_loader)
+        avg_test_loss = evaluate(model, test_loader)
+
+        print(f"epoch: {epoch}, total_loss: {total_loss}, avg_loss: {avg_loss/batch_idx}, val_loss: {avg_val_loss}, test_loss: {avg_test_loss}")
+
+def avg_loss_calc(pred, target):
+    """
+    Args:
+        pred: (tensor) with sized [#data, ]
+        target: (tensor) with sized [#data, ]
+    """
+    num_data = pred.size(0)
+    total_loss = 0
+    for i in range(num_data):
+        total_loss += abs(pred[i] - target[i])
+
+    return total_loss/num_data
+
+def evaluate(model, val_loader):
+    model.eval()
+    avg_loss = 0
+    for batch_idx, (inputs, labels) in enumerate(val_loader):
+        pred = model(inputs)
+        avg_loss += avg_loss_calc(pred, labels).item()
+    
+    return avg_loss/batch_idx
+
+
+if __name__ == "__main__":
+    train()
