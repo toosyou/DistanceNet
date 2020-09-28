bf9fa39463ca0e730bc26b9a5c9b8ca79e43e391
toosyou <toosyou.tw@gmail.com>
2020-09-28 14:23:22 +0800

    train with sparse data

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from tensorflow.keras import models
 from tensorflow.keras.layers import Conv1D, MaxPooling1D, Dense, GlobalAveragePooling1D
 from tensorflow.keras.callbacks import EarlyStopping
 
-from data.gen_data import gen_distance_peak_data
+from data.gen_data import gen_distance_peak_data, gen_distance_peak_data_choice
 
 def get_model():
     model = models.Sequential()
@@ -28,11 +28,19 @@ def get_model():
     return model
 
 if __name__ == '__main__':
+    '''
     X, y = gen_distance_peak_data()
     X = np.swapaxes(X, 1, 2)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.25, random_state=42)
+    '''
+
+    X_train, y_train = gen_distance_peak_data_choice(100000)
+    X_valid, y_valid = gen_distance_peak_data(1000)
+
+    X_train = np.swapaxes(X_train, 1, 2)
+    X_valid = np.swapaxes(X_valid, 1, 2)
 
     model = get_model()
     model.summary()
