20e3926aacc852a83fb12b919ce14be8228b942a
writerwriter <catchingfire80@gmail.com>
2020-10-28 19:24:13 +0800

    Update: visualization methods

--- utils/visualization.py
+++ utils/visualization.py
@@ -4,13 +4,14 @@ import seaborn as sns
 from matplotlib.patches import ConnectionPatch
 from matplotlib import transforms
 
-def draw(signal1, signal2, attention):
+def atten_pair(signal1, signal2, attention):
     """
     Args:
         signal1: (Array) with sized [data_length]
         signal2: (Array) with sized [data_length]
         attention: (Array) with sized [data_length, data_length]
     """
+    plt.clf()
     signal_length = signal1.shape[0]
     fig = plt.figure(figsize=(20, 10))
     ax1 = fig.add_subplot(2, 1, 1)
@@ -39,34 +40,35 @@ def draw(signal1, signal2, attention):
     
     return plt
 
-def atten_heatmap(signal1, signal2, attention):
-    fig, axes = plt.subplots(2,2, gridspec_kw={'width_ratios': [1, 10], 'height_ratios':[1, 10]}, figsize=(20, 20))
+def atten_heatmap(signal1, signal2, attention, figsize=(20, 20)):
+    plt.clf()
+    fig, axes = plt.subplots(2,2, gridspec_kw={'width_ratios': [1, 10], 'height_ratios':[1, 10]}, figsize=figsize)
     axes[0,0].set_visible(False)
     #axes[0,1].set_visible(False)
     #axes[1,0].set_visible(False)
     #axes[1,1].set_visible(False)
 
-    axes[0, 1].plot(signal1)
-    axes[0, 1].plot(signal2)
+    axes[0, 1].plot(signal1, alpha=0.5)
+    axes[0, 1].plot(signal2, alpha=0.5)
     axes[0, 1].axis('off')
     axes[0, 1].margins(0.005)    
     #axes[1, 2].plot(signal2)
     #axes[1, 2].axis('off')
     #axes[1, 2].margins(0.005)
 
-    rot = transforms.Affine2D().rotate_deg(90)
+    rot = transforms.Affine2D().rotate_deg(270)
 
     base = axes[1,0].transData
-    axes[1, 0].plot(np.arange(attention.shape[1]), signal2, transform=rot+base)
-    axes[1, 0].plot(np.arange(attention.shape[1]), signal1, transform=rot+base)
+    axes[1, 0].plot(signal1, transform=rot+base, alpha=0.5)
+    axes[1, 0].plot(signal2, transform=rot+base, alpha=0.5)
     axes[1, 0].axis('off')
-    axes[1, 0].margins(0.005)    
+    axes[1, 0].margins(0.005)
     #base = axes[2,1].transData
     #axes[2, 1].plot(np.arange(attention.shape[1]), signal1, transform=rot+base)
     #axes[2, 1].axis('off')
     #axes[2, 1].margins(0.005)
 
-    sns.heatmap(attention, ax=axes[1,1], cbar=False, xticklabels=False, yticklabels=False,cmap='Reds')
+    sns.heatmap(attention, ax=axes[1,1], cbar=False, xticklabels=False, yticklabels=False,cmap='Greens', robust=False)
     plt.margins(0)
     plt.subplots_adjust(wspace=0.01, hspace=0.01)
 
